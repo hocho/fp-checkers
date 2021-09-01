8c2e611b5645a7f766c56f2c58332a0a684609f0
Homi Chothia <homi@chothia.com>
2021-08-31 18:09:01 -0700

    Create Game module

--- app/Main.hs
+++ app/Main.hs
@@ -1,63 +1,10 @@
 module Main where
 
-import Board
+import Game
     (
-        Board
-    ,   Player
-    ,   Color
-    ,   color
-    ,   otherPlayer
-    ,   boardInitial
-    ,   boardDisplay
-    ,   player1
-    ,   player2
+        gamePlay
     )
-
-import Move
-    (   Move(..)
-    ,   Move_(..)
-    )
-
-import MoveSingle
-import MoveJump
-
-import Data.List
 
 main :: IO ()
 main = do
-    let
-        board = boardInitial()
-    do
-        boardDisplay board
-        playGame
-            board
-            player1
-
-movesGet :: Board -> Player -> [Move]
-movesGet board player = 
-    (map Move $ movesJumpGet board player)
-    ++ 
-    (map Move $ movesSingleGet board player)
-
-playGame :: Board -> Player -> IO()
-playGame board player = do
-    let
-        moves = movesGet board player
-        move = uncons moves
-    do
-        case move of
-            Nothing ->
-                return ()
-            Just ((Move nextMove), _) -> do
-                let
-                    newBoard = movePlay board nextMove
-                    nextPlayer = 
-                        if moveName nextMove == "MoveSingle" 
-                            then otherPlayer player
-                            else player
-                do
-                    putStr $ show (color player) ++ " "
-                    putStrLn $ moveShow nextMove
-                    putStrLn ""
-                    boardDisplay newBoard
-                    playGame newBoard nextPlayer
+    gamePlay
---
+++ src/Game.hs
@@ -0,0 +1,63 @@
+module Game where
+
+import Board
+    (
+        Board
+    ,   Player
+    ,   Color
+    ,   color
+    ,   otherPlayer
+    ,   boardInitial
+    ,   boardDisplay
+    ,   player1
+    ,   player2
+    )
+
+import Move
+    (   Move(..)
+    ,   Move_(..)
+    )
+
+import MoveSingle
+import MoveJump
+
+import Data.List
+
+gamePlay :: IO ()
+gamePlay = do
+    let
+        board = boardInitial()
+    do
+        boardDisplay board
+        playGame
+            board
+            player1
+
+movesGet :: Board -> Player -> [Move]
+movesGet board player = 
+    (map Move $ movesJumpGet board player)
+    ++ 
+    (map Move $ movesSingleGet board player)
+
+playGame :: Board -> Player -> IO()
+playGame board player = do
+    let
+        moves = movesGet board player
+        move = uncons moves
+    do
+        case move of
+            Nothing ->
+                return ()
+            Just ((Move nextMove), _) -> do
+                let
+                    newBoard = movePlay board nextMove
+                    nextPlayer = 
+                        if moveName nextMove == "MoveSingle" 
+                            then otherPlayer player
+                            else player
+                do
+                    putStr $ show (color player) ++ " "
+                    putStrLn $ moveShow nextMove
+                    putStrLn ""
+                    boardDisplay newBoard
+                    playGame newBoard nextPlayer
